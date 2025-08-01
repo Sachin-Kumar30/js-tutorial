18d665a54117504944e43897393f33a2fcc01e72
Sachin-Kumar30 <sachinkuar4548@gmail.com>
2025-08-01 14:10:11 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,27 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber=33;
 
 let stringNumber= String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************** Operation **********************
+
+let value = 3;
+let negvalue= - value;
+console.log(negvalue);
+
+let str1="hello";
+let str2= " Sachin";
+// let str3= str1 + str2;  // another way -> down there 
+// console.log(str1 +str2);
+
+// console.log(1 + 2 + "3"); // first operation then as a string
+// console.log(-true);
+// console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
