dcccd4c8e49d11f718b86ee3441ff9e1cae1ca7c
Sachin-Kumar30 <sachinkuar4548@gmail.com>
2025-08-01 13:26:24 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";   // treats all js code as newer version(by engineor machine)
+ 
+// alert(3+3)  // we are using"node js" not browser(its applicable there)
+
+console.log(3
+    +3)       // code readability should be high
+
+console.log("Radha")
+
+let name="sachin"
+let age=22
+let isLoggedIn=true
+
+// range of data types
+// number => 2 to pwer 53
+// bigint(for to biggger no)
+// string( prefer to use =>"")
+// boolean=>true/false
+// null => standalone value & it is object type
+// undefined=> not initiallised
+// symbol=> To check Uniqueness
+
+// objects=>
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)   // object
